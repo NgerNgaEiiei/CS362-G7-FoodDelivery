b91dcd8693ad6f1e5f7a5a7785e34b2dd04fa03b
teeieiyaaa <teerat.sris@dome.tu.ac.th>
2026-03-21 15:23:50 +0700

    V.1.0.1 - Add some code.

--- handlers/restaurant_handler.go
+++ handlers/restaurant_handler.go
@@ -1,1 +1,21 @@
-// จัดการการยืนยันออเดอร์จากฝั่งร้าน 
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"workflow-example.com/service"
+)
+
+type StudentHandler struct {
+	Service *service.StudentService
+}
+
+func (h *StudentHandler) GetStudents(c *gin.Context) {
+	students, err := h.Service.GetStudents()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, students)
+}
